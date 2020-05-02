4e53dd36ab219f03aa652e7bd34800352b909838
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-05-02 15:44:56 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-function printClock() {
-    
-    var clock = document.getElementById("clock");            
-    var currentDate = new Date();                            
-    var calendar = currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate() // 현재 날짜
-    var amPm = '오전'; // 초기값 AM
+var fs = require('fs')
+var express = require('express')
+var app = express();
+var timepage = require('./lib/timepage.js')
+var timecore = require('./public/time_core.js')
+var path = require('path')
+var helemt = require('helmet')
+var compression = require('compression')
+app.use(compression());
+app.use(express.static('public'))
+
+app.get('/', function(request, response){
+    var currentDate = new Date();       
     var currentHours = addZeros(currentDate.getHours()); 
     var currentMinute = addZeros(currentDate.getMinutes());
     var currentSeconds =  addZeros(currentDate.getSeconds());
-    
+    var amPm = '오전'; // 초기값 AM
     if(currentHours >= 12){ // 시간이 12보다 클 때 PM으로 세팅, 12를 빼줌
     	amPm = '오후';
     	currentHours = addZeros(currentHours - 12);
@@ -19,20 +26,22 @@ function printClock() {
         currentMinute = addZeros("0" + currentMinute);
         
     }
+    function addZeros(num, digit) { // 자릿수 맞춰주기
+        var zero = '';
+        num = num.toString();
+        if (num.length < digit) {
+          for (i = 0; i < digit - num.length; i++) {
+            zero += '0';
+          }
+        }
+        return zero + num;
+    }
+    var title = amPm + " " + currentHours + ":" + currentMinute
 
+    var main_index = timepage.HTML(title)
+    response.send(main_index)
+})
 
-    clock.innerHTML = "<span class='amPm'>"+ amPm+"</span> " +"<span class='timezone'>"+ currentHours+":"+currentMinute+":"+currentSeconds+"</span> "; //날짜를 출력해 줌
-    
-    setTimeout("printClock()",1000);         // 1초마다 printClock() 함수 호출
-}
-
-function addZeros(num, digit) { // 자릿수 맞춰주기
-	  var zero = '';
-	  num = num.toString();
-	  if (num.length < digit) {
-	    for (i = 0; i < digit - num.length; i++) {
-	      zero += '0';
-	    }
-	  }
-	  return zero + num;
-}
+app.listen(3000, function() {
+    console.log("Example app is running on port 3000");
+});
---
+++ public/time_core.js
@@ -0,0 +1,42 @@
+function printClock() {
+    
+    var clock = document.getElementById("clock");            
+    var currentDate = new Date();                            
+    var calendar = currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate() // 현재 날짜
+    var amPm = '오전'; // 초기값 AM
+    var currentHours = addZeros(currentDate.getHours()); 
+    var currentMinute = addZeros(currentDate.getMinutes());
+    var currentSeconds =  addZeros(currentDate.getSeconds());
+    
+    if(currentHours >= 12){ // 시간이 12보다 클 때 PM으로 세팅, 12를 빼줌
+    	amPm = '오후';
+    	currentHours = addZeros(currentHours - 12);
+    }
+    if (currentSeconds <= 9) {
+        currentSeconds = addZeros("0" + currentSeconds);
+    }
+    if (currentMinute <= 9){
+        currentMinute = addZeros("0" + currentMinute);
+        
+    }
+
+
+    clock.innerHTML = "<span class='amPm'>"+ amPm+"</span> " +"<span class='timezone'>"+ currentHours+":"+currentMinute+":"+currentSeconds+"</span> "; //날짜를 출력해 줌
+    
+    // setTimeout("printClock()",1000);
+    setTimeout("printClock()", 1000)  
+    if (currentSeconds == 0) {
+      setTimeout("location.reload()")
+    }
+}
+
+function addZeros(num, digit) { // 자릿수 맞춰주기
+	  var zero = '';
+	  num = num.toString();
+	  if (num.length < digit) {
+	    for (i = 0; i < digit - num.length; i++) {
+	      zero += '0';
+	    }
+	  }
+	  return zero + num;
+}
